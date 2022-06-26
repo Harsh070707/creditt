0ead79d91f67926a41b1db3bcfb51f44bb7394ac
Harsh Bansal <harsh.bansal@creolestudios.com>
2022-06-26 10:24:26 +0530

    Fixes:version changed of nvm

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,8 +4,7 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 #nvm install node
-
-nvm install 16
+nvm install v16.15.1
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/credit-app"
