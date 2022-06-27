ee49be35c7dbdb4e8c039200c59f151a663a224f
Harsh Bansal <harsh.bansal@creolestudios.com>
2022-06-27 16:38:13 +0530

    code merge

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,7 +12,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
-sudo npm i -g pm2
+npm i -g pm2
 
 npm install
 
