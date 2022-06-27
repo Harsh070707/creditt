d320b8146730604c41e69e424a2f96de088badf0
Harsh Bansal <harsh.bansal@creolestudios.com>
2022-06-27 16:32:06 +0530

    fixes:server return error

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,11 +12,17 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
+sudo npm i -g pm2
+
 npm install
 
 npm run build
 
-npm run start:prod
+pm2 start dist/main.js
+
+
+#npm run start:prod
+
 
 #start our node app in the background
 #node app.js > app.out.log 2> app.err.log < /dev/null & 
